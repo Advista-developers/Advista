029476d411da1352ee6d205d1b8b6f9e2608a88f
kedar-1401 <kedardhule14@gmail.com>
2024-11-05 22:14:59 +0530

    Added new URL backend

--- BackendAdvista/app.js
+++ BackendAdvista/app.js
@@ -74,12 +74,14 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(cors());
 app.use("/api", IndexRoute );
+app.get("/",(req,res)=>{
+  res.send("Hello Sir");
+})
+const PORT = process.env.PORT || 5000;
 
-// const PORT = process.env.PORT || 5000;
-
-// const server = app.listen(PORT, () => {
-//     console.log(`Example app listening on http://localhost:${PORT}`);
-//   });
+const server = app.listen(PORT, () => {
+    console.log(`Example app listening on http://localhost:${PORT}`);
+  });
 
   // const OpenAI = require('openai');  // Directly import the OpenAI class
 
